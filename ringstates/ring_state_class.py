09f2265329edb9f1951273b20fd537fdb59405ce
Nathaniel Puckett <napnathanielallenpuckett@gmail.com>
2025-10-19 18:56:22 -0400

    Simplified code, added get_all_data method to replace old get_lowest functionality

--- ringstates/ring_state_class.py
+++ ringstates/ring_state_class.py
@@ -2,7 +2,6 @@ import itertools as iter
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 from collections import Counter
 from copy import deepcopy
@@ -18,20 +17,18 @@ class RingState:
     Class for working with an n ring state in the context of photonic quantum computing.
     """
 
-    def __init__(self, nodes: int, debug: bool = False) -> None:
+    def __init__(self, nodes: int) -> None:
         """
         Parameters
         ----------
         nodes : int
             Number of nodes the graph state contains.
-        debug : bool
-            Used for optimizing, defaults to false.
         """
 
         self.nodes = nodes
         self.orderings = list()
         self.data = list()
-        self.debug = debug
+        self.maximum = factorial(self.nodes-1) / 2
 
     def __getitem__(self, index: int) -> list[list[int]]:
         """
@@ -49,62 +46,7 @@ class RingState:
         """
 
         return self.orderings[index]
-
-    def get_all_orderings(self) -> None:
-        """
-        Generates all possible orderings for the ring state.
-        """
-
-        time_start = time.perf_counter()
-
-        self.orderings = list()
-
-        permutations = [list(perm) for perm in iter.permutations(range(self.nodes)[1:], self.nodes-1)]
-        for permutation in permutations:
-            if permutation[0] == self.nodes-1: #reflective symmetry check 1
-                break
-            elif permutation[0] > permutation[-1]: #reflective symmetry check 2
-                pass
-            else:
-                permutation.insert(0, 0)
-                ordering = [[permutation[i], permutation[i+1]] for i in range(self.nodes-1)]
-                ordering.append([permutation[-1], permutation[0]])
-                self.orderings.append(ordering)
-        
-        time_end = time.perf_counter()
-        print(f"Time taken (orderings): {round((time_end-time_start) * 1000, 3)} ms") if self.debug else None
-
-    def get_random_orderings(self, num_orderings: int) -> None:
-        """
-        Generates a number of possible orderings for the ring state.
-        
-        Parameters
-        ----------
-        num_orderings : int
-            The number of random orderings to generate.
-        """
-        
-        time_start = time.perf_counter()
-
-        self.orderings = list()
-
-        if num_orderings > factorial(self.nodes-1) / 2:
-            num_orderings = factorial(self.nodes-1) / 2
-
-        while len(self.orderings) < num_orderings:
-            permutation = list(range(self.nodes)[1:])
-            shuffle(permutation)
-            if permutation[0]>permutation[-1] or permutation[0]==self.nodes-1: #reflective symmetry check
-                pass
-            else:
-                permutation.insert(0, 0)
-                ordering = [[permutation[i], permutation[i+1]] for i in range(self.nodes-1)]
-                ordering.append([permutation[-1], permutation[0]])
-                self.orderings.append(ordering)
-
-        time_end = time.perf_counter()
-        print(f"Time taken (orderings): {round((time_end-time_start) * 1000, 3)} ms") if self.debug else None
-
+    
     def add_ordering(self, ordering: list[list[int]]) -> None:
         """
         Adds an ordering to the list of orderings
@@ -116,6 +58,49 @@ class RingState:
         """
         
         self.orderings.append(ordering)
+
+    def get_all_orderings(self) -> None:
+        """
+        Generates all possible orderings for the ring state.
+        """
+
+        self.orderings = list()
+
+        permutations = [list(perm) for perm in iter.permutations(range(self.nodes)[1:], self.nodes-1)]
+        for permutation in permutations:
+            if permutation[0] == self.nodes-1: #reflective symmetry check 1
+                break
+            if permutation[0] > permutation[-1]: #reflective symmetry check 2
+                continue
+            permutation.insert(0, 0)
+            ordering = [[permutation[i], permutation[i+1]] for i in range(self.nodes-1)]
+            ordering.append([permutation[-1], permutation[0]])
+            self.orderings.append(ordering)
+
+    def get_random_orderings(self, num_orderings: int) -> None:
+        """
+        Generates a number of possible orderings for the ring state.
+        
+        Parameters
+        ----------
+        num_orderings : int
+            The number of random orderings to generate.
+        """
+
+        self.orderings = list()
+
+        if num_orderings > self.maximum:
+            num_orderings = self.maximum
+
+        while len(self.orderings) < num_orderings:
+            permutation = list(range(self.nodes)[1:])
+            shuffle(permutation)
+            if permutation[0]>permutation[-1] or permutation[0]==self.nodes-1: #reflective symmetry check
+                continue
+            permutation.insert(0, 0)
+            ordering = [[permutation[i], permutation[i+1]] for i in range(self.nodes-1)]
+            ordering.append([permutation[-1], permutation[0]])
+            self.orderings.append(ordering)
 
     def generate_data(self, ordering: list[list[int]], index: int) -> list[list]:
         """ 
@@ -146,6 +131,17 @@ class RingState:
             ]
         
         return ordering_data
+    
+    def get_all_data(self) -> None:
+        """ 
+        Generates data for all orderings
+        """
+
+        None if self.orderings else self.get_all_orderings()
+
+        for index, ordering in enumerate(self.orderings):
+            ord_data = self.generate_data(ordering, index)
+            self.data.append(ord_data)
 
     def get_lowest(self) -> int:
         """ 
@@ -157,38 +153,40 @@ class RingState:
             Index of the ordering containing the least number of CNOTs, Hadamards, and depth.
         """
 
-        time_start = time.perf_counter()
+        None if self.orderings else self.get_all_orderings()
 
-        self.get_all_orderings() if not self.orderings else None
-
-        #l_bound = int((2/(self.nodes - 1)) * len(self.orderings))
-        u_bound = int((2/(self.nodes - 2)) * len(self.orderings)) if self.nodes > 6 else len(self.orderings)
+        if self.nodes > 6 and len(self.orderings) == self.maximum:
+            u_bound = int((2/(self.nodes - 2)) * len(self.orderings))
+        else:
+            u_bound = len(self.orderings)
 
         for index, ordering in enumerate(self.orderings[0:u_bound]):
-
             g_state = GraphGen(self.graph(index))
-            
-            if g_state.num_emitters <= 2:
-
-                ord_data = self.generate_data(ordering, index)
-                
-                if index == 0:
+            if g_state.num_emitters > 2:
+                #all values are set to 0 in order to keep scatterplot method working
+                ord_data = [
+                    ["Index", index], 
+                    ["# CNOT", 0], 
+                    ["# Hadamard", 0], 
+                    ["# Phase", 0], 
+                    ["Depth", 0], 
+                    ["# Emitter", 0]
+                    ]
+                self.data.append(ord_data)
+                continue
+            ord_data = self.generate_data(ordering, index)
+            if index == 0:
+                l_index = index
+                l_data = ord_data
+            for i in range(1, 5):
+                if ord_data[i][1] < l_data[i][1]:
+                    print(f"Previous lowest index : {l_index} | New lowest index : {index}")
                     l_index = index
                     l_data = ord_data
-                else:
-                    for i in range(1, 5):
-                        if ord_data[i][1] < l_data[i][1]:
-                            print(f"Previous lowest index : {l_index} | New lowest index : {index}") if self.debug else None
-                            l_index = index
-                            l_data = ord_data
-                            break
-                        elif ord_data[i][1] > l_data[i][1]:
-                            break
-                    
-                self.data.append(ord_data)
-        
-        time_end = time.perf_counter()
-        print(f"Time taken (lowest): {round((time_end-time_start) * 1000, 3)} ms") if self.debug else None
+                    break
+                elif ord_data[i][1] > l_data[i][1]:
+                    break
+            self.data.append(ord_data)
 
         return l_index
 
